73802e47de8e27389033ae108c8469107ebf96f3
Shi Hao Tan <shihaotan91@gmail.com>
2016-09-24 22:40:14 +0800

    last question

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -124,7 +124,7 @@ makeBuddies();
 
 function beautifulStranger() {
 
-  aragorn[3].nodeValue = "Aragorn"
+  document.querySelectorAll('.buddies')[3].innerHTML = "Aragorn"
   // change the 'Strider' textnode to 'Aragorn'
 }
 beautifulStranger();
@@ -134,8 +134,43 @@ beautifulStranger();
 
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+// for (var i = 0; i < hobbits.length; i ++) {
+var assemble = document.querySelector('h1')
+var rivendell = document.querySelectorAll('h1')[1]
+// }
+
+// for (var i = 0; i < 1; i ++) {
+//   console.log(i)
+var indiHobbits = assemble.querySelector('ul')
+
+var movingAssemble = assemble.removeChild(indiHobbits)
+rivendell.appendChild(movingAssemble)
+
+// for (var i = 0; i < hobbits.length; i ++) {
+// 	console.log(i);
+//   var indiHobbits = assemble.querySelector('ul')
+//   console.log(indiHobbits);
+//
+//   var movingAssemble = assemble.removeChild(indiHobbits)
+//   rivendell.appendChild(movingAssemble)
+
+
+// var breakfast = document.getElementById('breakfast-foods')
+// var lunch = document.getElementById('lunch-foods')
+// var button = document.getElementById('brunch')
+
+
+// var lastList = breakfast.querySelector('li:last-child')
+//       var removedList = breakfast.removeChild(lastList)
+//
+//       lunch.appendChild(removedList)
+// var re
 
 }
+
+leaveTheShire();
+
+// var Rivendell = document.querySelectorAll('h1')[1];
 
 
 // Part 7
@@ -145,7 +180,33 @@ function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
+
+var theFellowShip = document.createElement('div')
+theFellowShip['id'] = "the-fellowship"
+var rivendell = document.querySelectorAll('h1')[1]
+
+rivendell.appendChild(theFellowShip)
+
+var assemble = document.querySelectorAll('ul')[1]
+var assemble2 = document.querySelector('ul')
+//
+for (var i = 0; i < hobbits.length; i ++) {
+    var indiHobbits = assemble.querySelector('li')
+    var movingAssemble = assemble.removeChild(indiHobbits)
+    theFellowShip.appendChild(movingAssemble)
+    alert(hobbits[i] + "have joined your party!")
+ }
+
+ for (var i = 0; i < buddies.length; i ++) {
+     var indiBuddies = assemble2.querySelector('li')
+     var movingAssemble = assemble2.removeChild(indiBuddies)
+     theFellowShip.appendChild(movingAssemble)
+     alert(buddies[i] + "have joined your party!")
+  }
+
+
 }
+forgeTheFellowShip();
 
 
 // Part 8
@@ -155,8 +216,16 @@ function theBalrog() {
   // change the 'Gandalf' textNode to 'Gandalf the White'
   // apply style to the element
   // make the background 'white', add a grey border
+
+var gandalf = document.querySelectorAll('.buddies')[0];
+gandalf.innerHTML = "Gandalf The White"
+
+gandalf.style.border = "10px grey solid"
+gandalf.style.background = "white"
+
 }
 
+theBalrog()
 
 // Part 9
 
@@ -165,7 +234,37 @@ function hornOfGondor() {
   // Boromir's been killed by the Uruk-hai!
   // put a linethrough on boromir's name
   // Remove Boromir from the Fellowship
+ alert("The horn of gondor has been blown!")
+ alert("The Boromir's been killed by the Uruk-Hai!")
+
+ var boromir = document.querySelectorAll('.buddies')[buddies.length-1]
+ boromir.style.textDecoration = "line-through"
+
+ var theFellowShip = document.getElementById('the-fellowship')
+
+ // var removeBoromir = theFellowShip.removeChild(boromir)
+
+ // var removeBoromir = document.querySelectorAll('li')[8]
+
+ // var frodo = document.querySelectorAll('li')[7]
+ // var theRing = document.getElementById('the-ring')
+ //
+ // var takeRing = frodo.removeChild(theRing)
+ // gollum.appendChild(takeRing)
+
+ //
+ // var buddy = document.querySelector('#the-fellowship')
+ // var lastList = breakfast.querySelector('li:last-child')
+ // var removedList = breakfast.removeChild(lastList)
+
+ // var theFellowShip = document.querySelector['#the-fellowship']
+ // theFellowShip.removeChild(list.childNodes[8])
+
+ // var list = document.getElementById("myList");
+ // list.removeChild(list.childNodes[0]);
 }
+
+hornOfGondor()
 
 
 // Part 10
@@ -173,7 +272,33 @@ function hornOfGondor() {
 function itsDangerousToGoAlone(){
   // take Frodo and Sam out of the fellowship and move them to Mordor
   // add a div with an id of 'mount-doom' to Mordor
+
+var theFellowShip = document.getElementById('the-fellowship')
+var mordor = document.querySelectorAll('h1')[2]
+
+for (var i = 0; i < 2 ; i ++) {
+    var frodonsam = theFellowShip.querySelector('li')
+    var movingHobbits = theFellowShip.removeChild(frodonsam)
+    mordor.appendChild(movingHobbits)
+ }
+
+ var mountDoom = document.createElement('div')
+ mountDoom.id = "mount-doom"
+ mordor.appendChild(mountDoom)
+
+ // for (var i = 0; i < hobbits.length ; i ++) {
+ //     var indiHobbits = assemble.querySelector('li')
+ //     var movingAssemble = assemble.removeChild(indiHobbits)
+ //     theFellowShip.appendChild(movingAssemble)
+ //     alert(hobbits[i] + "have joined your party!")
+ //  }
+ // var frodo = document.querySelectorAll('li')[0]
+ // var sam = document.querySelectorAll('li')[1]
+ // var assemble = document.querySelectorAll('ul')[1]
+
 }
+
+itsDangerousToGoAlone()
 
 
 // Part 11
@@ -182,13 +307,68 @@ function weWantsIt() {
   // Create a div with an id of 'gollum' and add it to Mordor
   // Remove the ring from Frodo and give it to Gollum
   // Move Gollum into Mount Doom
+  var gollum = document.createElement('div')
+  gollum.id = "gollum"
+
+  var frodo = document.querySelectorAll('li')[7]
+  var theRing = document.getElementById('the-ring')
+
+  var takeRing = frodo.removeChild(theRing)
+  gollum.appendChild(takeRing)
+
+  var mountDoom = document.getElementById('mount-doom')
+  mountDoom.appendChild(gollum)
+
 }
+
+weWantsIt()
 
 
 // Part 12
 
 function thereAndBackAgain() {
+
+  var gollum = document.getElementById('gollum')
+  var mountDoom = document.getElementById('mount-doom')
+  var removeGollum = mountDoom.removeChild(gollum)
+
+var theShire = document.getElementsByTagName('article')[0]
+
+for(var i = 0; i <= 1; i++) {
+  var list = document.getElementsByTagName('h1')[2]
+  var hobbits = document.querySelectorAll('li')[7]
+  var moveHobbits = list.removeChild(hobbits)
+  console.log(hobbits)
+}
+
+for(var i = 0; i <= 1; i++) {
+  var list2 = document.getElementById('the-fellowship')
+  var hobbits2 = document.querySelectorAll('li')[0]
+  var moveHobbits2 = list2.removeChild(hobbits2)
+ console.log(hobbits2)
+}
+
+var theShire = document.getElementsByTagName('article')[0]
+theShire.appendChild(moveHobbits)
+theShire.appendChild(moveHobbits2)
+
+
+  // var middleEarth = document.getElementById('middle-earth')
+  // var eye = document.querySelectorAll('article')[2]
+  // var removeEye = middleEarth.removeChild(eye)
+
+  // var mordor = document.querySelectorAll('h1')[2]
+
+  // var boromir = document.querySelectorAll('.buddies')[buddies.length-1]
+  //
+  // var theFellowShip = document.getElementById('the-fellowship')
+  //
+  // var removeBoromir = theFellowShip.removeChild(boromir)
+
+
   // remove Gollum and the Ring from the document
   // remove all the baddies from the document
   // Move all the hobbits back to the shire
 }
+
+thereAndBackAgain()
